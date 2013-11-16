c98cc2c347eb1d42cb8c359c649493bcf46d9505
Erika Nana <enana@hawaii.edu>
2013-11-15 16:20:49 -0800

    Add assignment

---
+++ app/controllers/assignments_controller.rb
@@ -0,0 +1,23 @@
+class AssignmentsController < ApplicationController
+    def index
+        @assignments = Assignment.all
+    end
+    def create
+        @assignment = Assignment.create!(params[:assignment])
+        redirect_to assignments_path
+    end
+    def show
+        id = params[:id]
+        @assignment = Assignment.find(id)
+        #render default view
+    end
+    def new
+        #renders the default view
+    end
+
+    def destroy
+        @assignment = Assignment.find(params[:id])
+        @assignment.destroy
+        redirect_to assignments_path
+    end
+end
